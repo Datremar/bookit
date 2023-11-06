ccffaf9559d86a06130022d34392b062ea038dc2
Mario Garrido <login@mariogarrido.pt>
2023-11-06 23:09:36 +0000

    fix: running tests

--- api/tests/auth_test.py
+++ api/tests/auth_test.py
@@ -21,12 +21,12 @@ class Test(unittest.TestCase):
     def test_get_auth_token(self):
         url = self.base_url + '/auth/login'
         data = {
-            'username': 'admin',
-            'password': 'admin'
+            "username": "admin",
+            "password": "admin"
         }
 
         # Send the request
-        response = requests.post(url, params=data)
+        response = requests.post(url, json=data)
 
         # Assert that the status code is 200
         self.assertEqual(response.status_code, 200)
